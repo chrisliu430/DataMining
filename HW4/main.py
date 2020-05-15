a78877f85c42fe0c33b6100eeae4e27f7b773e5d
chrisliu430 <hearnechris430@gmail.com>
2020-05-15 21:13:20 +0800

    Fix Bug

--- HW4/main.py
+++ HW4/main.py
@@ -10,10 +10,9 @@ class DBSCAN():
         self.width, self.height = np.shape(self.imgArr)
         self.cimgArr = np.full((self.width, self.height, 3), 0)
         self.valImg = np.full((self.width, self.height), 0)
-        self.stack = []
-        self.pointPos = [-1, -1]
+        self.pointsArr = [0, 0]
         self.pointVal = 2
-        self.distance = 0
+
     def Inverting(self):
         for x in range (self.height):
             for y in range (self.width):
@@ -21,78 +20,77 @@ class DBSCAN():
                     self.imgArr[y, x] = 0
                 else:
                     self.imgArr[y, x] = 1
+
     def Clustering(self):
+        # Pixel equal 1 is image pixel not background pixel
         for x in range (self.height):
             for y in range (self.width):
                 if (self.imgArr[y, x] == 1):
-                    returnVal = self.CheckPointValue(y, x)
-                    if (returnVal == 0 and self.CheckPointFact(y, x) == 1):
-                        self.valImg[y, x] = -1
-                    elif (returnVal == 0):
+                    self.edgeExist = self.CheckPointExist(y, x, 3)
+                    if (self.edgeExist == 0):
+                        self.imgArr[y, x] = 0
+        for x in range (self.height):
+            for y in range (self.width):
+                if (self.imgArr[y, x] == 1):
+                    self.edgeVal = self.CheckPointEdge(y, x, 10)
+                    if (self.edgeVal == 0):
                         self.valImg[y, x] = self.pointVal
                         self.pointVal += 1
+                        self.pointsArr.append(1)
                     else:
-                        self.valImg[y, x] = returnVal
+                        self.valImg[y, x] = self.edgeVal
+                        self.pointsArr[self.edgeVal] += 1
+                else:
+                    self.valImg[y, x] = -1
+
     def ToRGBArray(self):
         print(self.pointVal)
-        for k in range (2, self.pointVal + 1):
-            r = random.randint(1, 255)
-            g = random.randint(1, 255)
-            b = random.randint(1, 255)
+        for val in range (2, len(self.pointsArr)):
+            self.judge = False
+            r = random.randint(1,255)
+            g= random.randint(1,255)
+            b = random.randint(1,255)
+            if (self.pointsArr[val] < 10):
+                self.judge = True
             for x in range (self.height):
                 for y in range (self.width):
-                    if (self.valImg[y, x] == k):
+                    if (self.valImg[y, x] == -1 or (self.judge == True and self.valImg[y, x] == val)):
+                        self.cimgArr[y, x, 0] = 255
+                        self.cimgArr[y, x, 1] = 255
+                        self.cimgArr[y, x, 2] = 255
+                    elif (self.valImg[y, x] == val):
                         self.cimgArr[y, x, 0] = r
                         self.cimgArr[y, x, 1] = g
                         self.cimgArr[y, x, 2] = b
-                    elif (self.valImg[y, x] == -1):
-                        self.cimgArr[y, x, 0] = 255
-                        self.cimgArr[y, x, 1] = 255
-                        self.cimgArr[y, x, 2] = 255
-                    elif (self.valImg[y, x] == 0):
-                        self.cimgArr[y, x, 0] = 255
-                        self.cimgArr[y, x, 1] = 255
-                        self.cimgArr[y, x, 2] = 255
+
     def StorageIamge(self):
         cimg = Image.fromarray(self.cimgArr.astype('uint8')).convert('RGB')
         cimg.save("convert.bmp")
-    def CheckPointValue(self, startY, startX):
+        
+    def CheckPointEdge(self, startY, startX, radis):
         self.val = 0
-        if (self.valImg[startY - 1, startX - 1] != 0):
-            self.val = self.valImg[startY - 1, startX - 1]
-        elif (self.valImg[startY - 1, startX] != 0):
-            self.val = self.valImg[startY - 1, startX]
-        elif (self.valImg[startY - 1, startX + 1] != 0):
-            self.val = self.valImg[startY - 1, startX + 1]
-        elif (self.valImg[startY, startX - 1] != 0):
-            self.val = self.valImg[startY, startX - 1]
-        elif (self.valImg[startY, startX + 1] != 0):
-            self.val = self.valImg[startY, startX + 1]
-        elif (self.valImg[startY + 1, startX - 1] != 0):
-            self.val = self.valImg[startY + 1, startX - 1]
-        elif (self.valImg[startY + 1, startX] != 0):
-            self.val = self.valImg[startY + 1, startX]
-        elif (self.valImg[startY + 1, startX + 1] != 0):
-            self.val = self.valImg[startY + 1, startX + 1]
+        for y in range (startY - radis, startY + radis + 1):
+            for x in range (startX - radis, startX + radis + 1):
+                if (x < 0 or y < 0 or x >= self.height or y >= self.width):
+                    break
+                if (self.valImg[y, x] != -1):
+                    self.val = self.valImg[y, x]
+                    break
+            if (self.val != 0):
+                break
         return self.val
-    def CheckPointFact(self, startY, startX):
-        self.val = 1
-        if (self.imgArr[startY - 1, startX - 1] == 1):
-            self.val = 0
-        elif (self.imgArr[startY - 1, startX] == 1):
-            self.val = 0
-        elif (self.imgArr[startY - 1, startX + 1] == 1):
-            self.val = 0
-        elif (self.imgArr[startY, startX - 1] == 1):
-            self.val = 0
-        elif (self.imgArr[startY, startX + 1] == 1):
-            self.val = 0
-        elif (self.imgArr[startY + 1, startX - 1] == 1):
-            self.val = 0
-        elif (self.imgArr[startY + 1, startX] == 1):
-            self.val = 0
-        elif (self.imgArr[startY + 1, startX + 1] == 1):
-            self.val = 0
+
+    def CheckPointExist(self, startY, startX, radis):
+        self.val = 0
+        for y in range (startY - radis, startY + radis + 1):
+            for x in range (startX - radis, startX + radis + 1):
+                if (x < 0 or y < 0 or x >= self.height or y >= self.width):
+                    break
+                if (self.imgArr[y, x] != 0 and x != startX and y != startY):
+                    self.val = 1
+                    break
+            if (self.val):
+                break
         return self.val
 
 if __name__ == "__main__":
