8fe7f0b0e5d2588af5c4c8c8e67bac14fc2d5cf3
chrisliu430 <hearnechris430@gmail.com>
2020-05-18 22:16:45 +0800

    Fix Parameter Error

--- HW4/main.py
+++ HW4/main.py
@@ -7,95 +7,94 @@ class DBSCAN():
         self.fileName = fileName
         self.img = Image.open(fileName)
         self.imgArr = np.array(self.img)
-        self.width, self.height = np.shape(self.imgArr)
-        self.cimgArr = np.full((self.width, self.height, 3), 0)
-        self.valImg = np.full((self.width, self.height), 0)
-        self.pointsArr = [0, 0]
+        self.height, self.width = np.shape(self.imgArr)
+        self.cimgArr = np.full((self.height, self.width, 3), 0)
+        self.valImg = np.full((self.height, self.width), 0)
+        self.valuesArr = [0, 0]
         self.pointVal = 2
 
-    def Inverting(self):
-        for x in range (self.height):
-            for y in range (self.width):
+    def inverting(self):
+        for y in range (self.height):
+            for x in range (self.width):
                 if (self.imgArr[y, x]):
                     self.imgArr[y, x] = 0
                 else:
                     self.imgArr[y, x] = 1
-
-    def Clustering(self):
-        # Pixel equal 1 is image pixel not background pixel
-        for x in range (self.height):
-            for y in range (self.width):
-                if (self.imgArr[y, x] == 1):
-                    self.edgeExist = self.CheckPointExist(y, x, 3)
-                    if (self.edgeExist == 0):
+    
+    def clustering(self):
+        for y in range (self.height):
+            for x in range (self.width):
+                if (self.imgArr[y, x]):
+                    pointIsExist = self.discretecheck(y, x, 3)
+                    if (not(pointIsExist)):
                         self.imgArr[y, x] = 0
-        for x in range (self.height):
-            for y in range (self.width):
-                if (self.imgArr[y, x] == 1):
-                    self.edgeVal = self.CheckPointEdge(y, x, 10)
-                    if (self.edgeVal == 0):
+        for y in range (self.height):
+            for x in range (self.width):
+                if (self.imgArr[y, x]):
+                    value = self.scanpointsvalue(y, x, 10)
+                    if (value == 0):
                         self.valImg[y, x] = self.pointVal
                         self.pointVal += 1
-                        self.pointsArr.append(1)
+                        self.valuesArr.append(1)
                     else:
-                        self.valImg[y, x] = self.edgeVal
-                        self.pointsArr[self.edgeVal] += 1
+                        self.valImg[y, x] = value
+                        self.valuesArr[value] += 1
                 else:
                     self.valImg[y, x] = -1
-
-    def ToRGBArray(self):
+      
+    def toRGBarray(self):
         print(self.pointVal)
-        for val in range (2, len(self.pointsArr)):
-            self.judge = False
-            r = random.randint(1,255)
-            g= random.randint(1,255)
-            b = random.randint(1,255)
-            if (self.pointsArr[val] < 10):
-                self.judge = True
-            for x in range (self.height):
-                for y in range (self.width):
-                    if (self.valImg[y, x] == -1 or (self.judge == True and self.valImg[y, x] == val)):
+        for val in range (2, len(self.valuesArr)):
+            r = random.randint(1, 255)
+            g = random.randint(1, 255)
+            b = random.randint(1, 255)
+            if (self.valuesArr[val] < 10):
+                r = g = b = -1
+            for y in range (self.height):
+                for x in range (self.width):
+                    if (self.valImg[y, x] == -1):
                         self.cimgArr[y, x, 0] = 255
                         self.cimgArr[y, x, 1] = 255
                         self.cimgArr[y, x, 2] = 255
-                    elif (self.valImg[y, x] == val):
+                    if (self.valImg[y, x] == val):
                         self.cimgArr[y, x, 0] = r
                         self.cimgArr[y, x, 1] = g
                         self.cimgArr[y, x, 2] = b
 
-    def StorageIamge(self):
+    def storageimage(self):
         cimg = Image.fromarray(self.cimgArr.astype('uint8')).convert('RGB')
         cimg.save("convert.bmp")
-        
-    def CheckPointEdge(self, startY, startX, radis):
-        self.val = 0
-        for y in range (startY - radis, startY + radis + 1):
-            for x in range (startX - radis, startX + radis + 1):
-                if (x < 0 or y < 0 or x >= self.height or y >= self.width):
-                    break
-                if (self.valImg[y, x] != -1):
-                    self.val = self.valImg[y, x]
-                    break
-            if (self.val != 0):
-                break
-        return self.val
 
-    def CheckPointExist(self, startY, startX, radis):
-        self.val = 0
-        for y in range (startY - radis, startY + radis + 1):
-            for x in range (startX - radis, startX + radis + 1):
-                if (x < 0 or y < 0 or x >= self.height or y >= self.width):
+    # --- SCAN
+    def discretecheck(self, startY, startX, redis):
+        judge = False
+        for y in range (startY - redis, startY + redis + 1):
+            for x in range (startX - redis, startX + redis + 1):
+                if (y < 0 or x < 0 or x >= self.width or y >= self.height):
                     break
                 if (self.imgArr[y, x] != 0 and x != startX and y != startY):
-                    self.val = 1
-                    break
-            if (self.val):
+                    judge = True
+            if (judge):
                 break
-        return self.val
+        return judge
 
+    def scanpointsvalue(self, startY, startX, redis):
+        value = 0
+        for y in range (startY - redis, startY + redis + 1):
+            for x in range (startX - redis, startX + redis + 1):
+                if (x < 0 or y < 0 or x >= self.width or y >= self.height):
+                    break
+                if (self.valImg[y, x] != -1):
+                    value = self.valImg[y, x]
+                    break
+            if (value != 0):
+                break
+        return value
+    # ---
+    
 if __name__ == "__main__":
     ds = DBSCAN("./test.bmp")
-    ds.Inverting()
-    ds.Clustering()
-    ds.ToRGBArray()
-    ds.StorageIamge()
+    ds.inverting()
+    ds.clustering()
+    ds.toRGBarray()
+    ds.storageimage()
