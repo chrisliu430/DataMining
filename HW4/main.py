a5370072ba08a84bcc7cf59f1e5adf9c850e4859
chrisliu430 <hearnechris430@gmail.com>
2020-05-20 22:24:42 +0800

    Fix nonclusstering bug

--- HW4/main.py
+++ HW4/main.py
@@ -42,20 +42,27 @@ class DBSCAN():
             for x in range (self.width):
                 if (not(self.imgArr[y, x])):
                     self.valImg[y, x] = 0
-                elif (self.imgArr[y, x]):
-                    self.valImg[y, x] = self.pointVal
-                    self.valuesArr.append(1)
-                    self.pointVal += 1
-                    self.RecursionNearPoints(y + 1, x, 3, self.pointVal - 1)
+                elif (self.imgArr[y, x] and self.valImg[y, x] == -1):
+                    rVal = self.CatchNearPointsValue(y, x, 7)
+                    if (not(rVal)):
+                        self.valImg[y, x] = self.pointVal
+                        self.valuesArr.append(1)
+                        self.pointVal += 1
+                        self.RecursionNearPoints(y + 1, x, 13, self.pointVal - 1)
+                    else:
+                        self.valImg[y, x] = rVal
+                        self.valuesArr[rVal] += 1
+                        self.RecursionNearPoints(y + 1, x, 13, self.pointVal - 1)
 
-    def RecursionNearPoints(self, centerY, cencterX, redis, value):
-        for x in range (cencterX - redis, cencterX + redis + 1):
-            if (self.imgArr[centerY, x]):
+    def RecursionNearPoints(self, centerY, centerX, redis, value):
+        for x in range (centerX - redis, centerX + redis + 1):
+            if (x < 0 or x >= self.width):
+                break
+            if (self.imgArr[centerY, x] and self.valImg[centerY, x] == -1):
                 self.valImg[centerY, x] = value
                 self.valuesArr[value] += 1
                 self.RecursionNearPoints(centerY + 1, x, redis, value)
-                return
-
+        
     def ScanNearPoints(self, centerY, centerX, redis):
         exist = False
         for y in range (centerY - redis, centerY + redis + 1):
@@ -68,10 +75,39 @@ class DBSCAN():
             if (exist):
                 break
         return exist
+    
+    def CatchNearPointsValue(self, centerY, centerX, redis):
+        rVal = 0
+        for y in range (centerY - redis, centerY + redis + 1):
+            for x in range (centerX - redis, centerX + redis + 1):
+                if (y < 0 or y >= self.height or x < 0 or x >= self.width):
+                    break
+                if (self.valImg[y, x] != -1 and self.valImg[y, x] != 0):
+                    rVal = self.valImg[y, x]
+                    break
+            if (rVal):
+                break
+        return rVal
 
     def ToRGBArray(self):
-        print(self.valuesArr[1])
-        print(self.pointVal)
+        print(len(self.valuesArr))
+        for sVal in range (1, len(self.valuesArr)):
+            judge = False
+            r = random.randint(1,255)
+            g = random.randint(1,255)
+            b = random.randint(1,255)
+            if (self.valuesArr[sVal] < 10):
+                judge = True
+            for y in range (self.height):
+                for x in range (self.width):
+                    if (self.valImg[y, x] == 0 or (self.valImg[y, x] == sVal and judge)):
+                        self.cImgArr[y, x, 0] = 255
+                        self.cImgArr[y, x, 1] = 255
+                        self.cImgArr[y, x, 2] = 255
+                    elif (self.valImg[y, x] == sVal):
+                        self.cImgArr[y, x, 0] = r
+                        self.cImgArr[y, x, 1] = g
+                        self.cImgArr[y, x, 2] = b
 
     # For Test
     def GaryImage(self):
@@ -93,3 +129,6 @@ if __name__ == "__main__":
     ds = DBSCAN("./test.bmp")
     ds.Start()
     ds.CreateNewImage("./convert.bmp")
+    # ds = DBSCAN("./test2.bmp")
+    # ds.Start()
+    # ds.CreateNewImage("./convert2.bmp")
