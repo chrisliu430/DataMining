bf20dada434c865d2b66f133375cb1058fef0bd2
chrisliu430 <hearnechris430@gmail.com>
2020-06-03 04:14:15 +0800

    Update new algorithm and after image

--- HW4/main.py
+++ HW4/main.py
@@ -1,9 +1,10 @@
 from PIL import Image
 import numpy as np
 import random
+import math
 
 class DBSCAN():
-    def __init__(self, fileName):
+    def __init__(self, fileName, minNumber, redis):
         self.img = Image.open(fileName)
         self.imgArr = np.array(self.img)
         self.width, self.height = self.img.size
@@ -11,6 +12,8 @@ class DBSCAN():
         self.cImgArr = np.full((self.height, self.width, 3), -1)
         self.valuesArr = [0]
         self.pointVal = 1
+        self.minNumber = minNumber
+        self.redis = redis
 
     def Start(self):
         self.Inverting()
@@ -21,7 +24,7 @@ class DBSCAN():
     def CreateNewImage(self, newName):
         cimg = Image.fromarray(self.cImgArr.astype('uint8')).convert('RGB')
         cimg.save(newName)
-    
+
     # Hiding Layer ---
 
     # Finish ---
@@ -36,35 +39,42 @@ class DBSCAN():
     def RemovePoints(self):
         for y in range (self.height):
             for x in range (self.width):
-                if (self.imgArr[y, x].any() and not(self.ScanNearPoints(y, x, 2))):
-                    self.ClearNearPoints(y, x, 2)
-
-    def ClearNearPoints(self, centerY, centerX, redis):
-        for y in range (centerY - redis, centerY + redis + 1):
-            for x in range (centerX - redis, centerX + redis + 1):
-                if (y < 0 or y >= self.height or x < 0 or x >= self.width):
-                    break
-                else:
+                if (self.imgArr[y, x].any() and not(self.ScanNearPoints(y, x, self.redis))):
                     self.imgArr[y, x] = 0
 
     def ScanNearPoints(self, centerY, centerX, redis):
         exist = False
         counter = 0
+        distance = 0
         for y in range (centerY - redis, centerY + redis + 1):
             for x in range (centerX - redis, centerX + redis + 1):
+                distance = int(math.sqrt(math.pow((y - centerY), 2) + math.pow((x - centerX), 2)))
                 if (y < 0 or y >= self.height or x < 0 or x >= self.width):
                     break
-                elif (y == centerY and x == centerX):
-                    continue
-                elif (self.imgArr[y, x].any()):
+                elif ((y == centerY and x == centerX) or (self.imgArr[y, x].any() and distance <= redis)):
                     counter += 1
-                if (counter > 8):
+                if (counter > 1):
                     exist = True
                     break
             if (exist):
                 break
         return exist
-        
+
+    def ClearNearPoints(self, centerY, centerX, redis):
+        only = True
+        distance = 0
+        for y in range (centerY - redis, centerY + redis + 1):
+            for x in range (centerX - redis, centerX + redis + 1):
+                distance = int(math.sqrt(math.pow((y - centerY), 2) + math.pow((x - centerX), 2)))
+                if (y < 0 or y >= self.height or x < 0 or x >= self.width):
+                    break
+                elif (y == centerY and x == centerX):
+                    continue
+                elif (distance <= redis):
+                    only = False
+        if (only):
+            self.imgArr[centerY, centerX] = 0
+
     def ToRGBArray(self):
         print(len(self.valuesArr))
         for sVal in range (1, len(self.valuesArr)):
@@ -72,8 +82,6 @@ class DBSCAN():
             r = random.randint(1,255)
             g = random.randint(1,255)
             b = random.randint(1,255)
-            if (self.valuesArr[sVal] < 50):
-                judge = True
             for y in range (self.height):
                 for x in range (self.width):
                     if (self.valImg[y, x] == 0 or (self.valImg[y, x] == sVal and judge)):
@@ -86,75 +94,118 @@ class DBSCAN():
                         self.cImgArr[y, x, 2] = b
     # Finish End
 
+    def CheckPassMinNumber(self, centerY, centerX, redis):
+        passStatus = False
+        counter = 0
+        distance = 0
+        for y in range (centerY - redis, centerY + redis + 1):
+            for x in range (centerX - redis, centerX + redis + 1):
+                distance = int(math.sqrt(math.pow((y - centerY), 2) + math.pow((x - centerX), 2)))
+                if (y < 0 or y >= self.height or x < 0 or x >= self.width):
+                    break
+                elif ((y == centerY and x == centerX) or (self.imgArr[y, x].any() and distance <= redis)):
+                    counter += 1
+                if (counter >= self.minNumber):
+                    passStatus = True
+                    break
+            if (passStatus):
+                break
+        return passStatus
+
+    def ChangePassYAxisValue(self, centerY, centerX, redis, value):
+        distance = 0
+        for y in range (centerY, centerY - redis - 1, -1):
+            distance = int(math.sqrt(math.pow((y - centerY), 2)))
+            if (y < 0 or y >= self.height or centerX < 0 or centerX >= self.width):
+                break
+            elif (y == centerY):
+                continue
+            elif (self.imgArr[y, centerX].any() and distance <= redis):
+                self.valImg[y, centerX] = value
+                self.ChangePassYAxisValue(y, centerX, redis, value)
+                self.ChangePassXAxisValue(y, centerX, redis, value)
+            else:
+                return
+
+    def ChangePassXAxisValue(self, centerY, centerX, redis, value):
+        distance = 0
+        for x in range (centerX, centerX - redis - 1, -1):
+            distance = int(math.sqrt(math.pow((x - centerX), 2)))
+            if (centerY < 0 or centerY >= self.height or x < 0 or x >= self.width):
+                break
+            elif (x == centerX):
+                continue
+            elif (self.imgArr[centerY, x].any() and distance <= redis):
+                self.valImg[centerY, x] = value
+                self.ChangePassXAxisValue(centerY, x, redis, value)
+            else:
+                return
+
     def Clustering(self):
         for y in range (self.height):
             for x in range (self.width):
                 if (not(self.imgArr[y, x].any())):
                     self.valImg[y, x] = 0
-                elif (self.imgArr[y, x].any() and self.valImg[y, x] == -1):
-                    rVal = self.GetNearPointsValue(y, x, 5)
-                    if (not(rVal)):
-                        rVal = self.pointVal
-                        self.valImg[y, x] = rVal
-                        self.valuesArr.append(1)
-                        self.pointVal += 1
+                elif (self.imgArr[y, x].any()):
+                    if (self.CheckPassMinNumber(y, x, self.redis)):
+                        rVal = self.GetNearPointsValue(y, x, self.redis)
+                        if (rVal == 0):
+                            rVal = self.pointVal
+                            self.valImg[y, x] = rVal
+                            self.valuesArr.append(1)
+                            self.pointVal += 1
+                        else:
+                            self.valImg[y, x] = rVal
+                            self.valuesArr[rVal] += 1
                     else:
-                        self.valImg[y, x] = rVal
-                        self.valuesArr[rVal] += 1
-                    self.RecursionCircle(y + 1, x, 3, rVal)
-    
-    def RecursionCircle(self, centerY, centerX, redis, value):
-        stepY = False
-        for x in range (centerX - redis, centerX + redis + 1):
-            if (centerY < 0 or centerY >= self.height or x < 0 or x >= self.width):
-                break
-            elif (self.imgArr[centerY, x].any() and self.valImg[centerY, x] == -1):
-                stepY = True
-                self.valImg[centerY, x] = value
-                self.valuesArr[value] += 1
-                self.RecursionSearchLine(centerY, x, redis, value)
-            if (stepY):
-                stepY = False
-                self.RecursionCircle(centerY + 1, x, redis, value)
+                        rVal = self.GetNearPointsValue(y, x, self.redis)
+                        if (rVal == 0):
+                            self.valImg[y, x] = 0
+                        else:
+                            self.valImg[y, x] = rVal
+                        self.ChangePassYAxisValue(y, x, self.redis, rVal)
+                    self.ChangePassXAxisValue(y, x, self.redis, rVal)
 
-    def RecursionSearchLine(self, centerY, centerX, redis, value):
-        for x in range (centerX - redis, centerX + redis + 1):
-            if (centerY < 0 or centerY >= self.height or x < 0 or x >= self.width):
-                break
-            elif (self.imgArr[centerY, x].any() and self.valImg[centerY, x] == -1):
-                self.valImg[centerY, x] = value
-                self.valuesArr[value] += 1
-                self.RecursionSearchLine(centerY, x, redis, value)
-                
     def GetNearPointsValue(self, centerY, centerX, redis):
         rVal = 0
-        for y in range (centerY, centerY - redis, -1):
+        distance = 0
+        for y in range (centerY - redis, centerY + redis + 1):
             for x in range (centerX - redis, centerX + redis + 1):
+                distance = int(math.sqrt(math.pow((y - centerY), 2) + math.pow((x - centerX), 2)))
                 if (y < 0 or y >= self.height or x < 0 or x >= self.width):
                     break
-                elif (self.valImg[y, x] != 0 and self.valImg[y, x] != -1):
+                elif (self.valImg[y, x] != 0 and self.valImg[y, x] != -1 and distance <= redis):
                     rVal = self.valImg[y, x]
                     break
             if (rVal):
                 break
-        if (not(rVal)):
-            for y in range (centerY, centerY + redis + 1):
-                for x in range (centerX - redis, centerX + redis + 1):
-                    if (y < 0 or y >= self.height or x < 0 or x >= self.width):
-                        break
-                    elif (self.valImg[y, x] != 0 and self.valImg[y, x] != -1):
-                        rVal = self.valImg[y, x]
-                        break
-                if (rVal):
-                    break
         return rVal
 
     # --- --- ---
 
 if __name__ == "__main__":
-    ds = DBSCAN("./test.bmp")
+    ds = DBSCAN("./test.bmp", 5, 1)
     ds.Start()
-    ds.CreateNewImage("./convert.bmp")
-    ds = DBSCAN("./test2.bmp")
-    ds.Start()
-    ds.CreateNewImage("./convert2.bmp")
+    ds.CreateNewImage("./convert_1_5.bmp")
+    # ds = DBSCAN("./test.bmp", 7, 1)
+    # ds.Start()
+    # ds.CreateNewImage("./convert_1_7.bmp")
+    # ds = DBSCAN("./test.bmp", 5, 3)
+    # ds.Start()
+    # ds.CreateNewImage("./convert_3_5.bmp")
+    # ds = DBSCAN("./test.bmp", 35, 5)
+    # ds.Start()
+    # ds.CreateNewImage("./convert_5_35.bmp")
+    # ---
+    # ds = DBSCAN("./test2.bmp", 5, 1)
+    # ds.Start()
+    # ds.CreateNewImage("./convert2_1_5.bmp")
+    # ds = DBSCAN("./test2.bmp", 7, 1)
+    # ds.Start()
+    # ds.CreateNewImage("./convert2_1_7.bmp")
+    # ds = DBSCAN("./test2.bmp", 5, 3)
+    # ds.Start()
+    # ds.CreateNewImage("./convert2_3_5.bmp")
+    # ds = DBSCAN("./test2.bmp", 35, 5)
+    # ds.Start()
+    # ds.CreateNewImage("./convert2_5_35.bmp")
